89be5c5cd89f3e8c560ccbd868ea94dab1c6e01f
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-22 21:56:35 -0300

    update

---
+++ Estudos/Javascript/ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(` a velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { // Condição Simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Diriga sempre usando cinto de segurança!')
