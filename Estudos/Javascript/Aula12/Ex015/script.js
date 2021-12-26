5016e618b1d2b2a915ffdc5d52908d167720b836
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-26 14:34:46 -0300

    update

--- Estudos/Javascript/Aula12/Ex015/script.js
+++ Estudos/Javascript/Aula12/Ex015/script.js
@@ -15,26 +15,35 @@ function verificar() {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'bebemenino.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemmenino.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'homemadulto.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'homemidoso.png')
             }
-        } else if (fanosex[1].checked) {
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'bebemenina.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemmenina.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'adultamulher.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'mulheridosa.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
