740f57cbadc5d411a4206fb6ffb985e4edad5724
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-26 12:52:26 -0300

    update

--- Estudos/Javascript/Ex014/script.js
+++ Estudos/Javascript/Ex014/script.js
@@ -1,9 +1,22 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    msg.innerHTML =`Agora são ${hora}:${minuto}` 
+    //var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML =`Agora são ${hora} Horas` 
+    if (hora >= 0 && hora < 12) {
+        //Bom Dia
+        img.src = 'manha.png'
+        document.body.style.background ='#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa Tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //Boa Noite
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
 }
 
