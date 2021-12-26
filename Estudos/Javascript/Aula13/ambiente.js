cfb7cf878ffe6df986a3d7e37ee23f111073df61
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-26 16:03:54 -0300

    update

---
+++ Estudos/Javascript/Aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // significa mesma coisa que c = c + 1
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // significa mesma coisa que c = c + 1
+}
+*/
