5eeda5cca1648051b1844570bbcaff3d6172ffd5
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-26 22:55:51 -0300

    update

--- Estudos/Javascript/Aula14ex/ex017/script.js
+++ Estudos/Javascript/Aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Ola!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor , digite um numero')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
