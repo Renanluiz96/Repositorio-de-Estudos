174aa29f905ad4d6c33bbe79c334ea4353044632
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-26 22:26:42 -0300

    update

--- Estudos/Javascript/Aula14ex/ex016/script.js
+++ Estudos/Javascript/Aula14ex/ex016/script.js
@@ -1,1 +1,33 @@
-alert('Ola!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let passo = document.getElementById('txtp')
+    let fim = document.getElementById('txtf')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel contar'
+    } else {
+        res.innerHTML = 'Contando : <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f){
+            //Contagem Crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem Regressiva
+            for (c = i; c >= f ; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }   
+}
