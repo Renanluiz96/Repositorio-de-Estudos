9d6c0c0ff8ec40f74a367b07d6e55dcdd162fb0d
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-27 22:07:37 -0300

    update

--- Estudos/Javascript/Aula15/vetornatela.js
+++ Estudos/Javascript/Aula15/vetornatela.js
@@ -8,8 +8,11 @@ console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
-*/
 
 for (let pos=0 ; pos<valores.length;pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]} `)
+} */
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
