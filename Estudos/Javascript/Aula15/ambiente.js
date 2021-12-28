c2f34ca16192879c643777c89555698df4e7d58e
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-27 23:11:28 -0300

    update

--- Estudos/Javascript/Aula15/ambiente.js
+++ Estudos/Javascript/Aula15/ambiente.js
@@ -1,4 +1,11 @@
 let num = [5, 8, 2, 9, 3]
-
+num.push(1)
+num.sort()
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`o primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor esta na posição ${pos}`)
+}
