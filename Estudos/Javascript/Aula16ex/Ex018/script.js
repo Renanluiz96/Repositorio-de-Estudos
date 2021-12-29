da3e4c230a24f3ebc1bcc408a2836ad863aa833e
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2021-12-28 23:09:06 -0300

    update

---
+++ Estudos/Javascript/Aula16ex/Ex018/script.js
@@ -0,0 +1,43 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1 ) {
+        return true
+    }else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} Adicionado`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor invalido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo , temos ${tot} numeros cadastrados</p>`
+    }
+}
