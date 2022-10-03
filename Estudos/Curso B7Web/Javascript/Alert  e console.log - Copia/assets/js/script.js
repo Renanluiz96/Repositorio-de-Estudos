bccfb3cc81a18e8b9d485e4740a3afd21f7d9297
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-10-02 23:31:22 -0300

    Operações em variaveis

---
+++ Estudos/Curso B7Web/Javascript/Alert  e console.log - Copia/assets/js/script.js
@@ -0,0 +1,10 @@
+//Operação matematica ele vai executar quando todos os valores forem tipo number 
+let x = 2
+x = x + 8
+console.log(x)
+
+// Agora com string ele vai fazer a concatenação ou seja ele vai juntar 2 string fazer 1 string só
+let nome = "Renan"
+let sobrenome = "luiz"
+let nomeCompleto = nome + '' + sobrenome // Aqui coloquei uma aspas simples ali porque se voce juntar os 2 ele vai ficar tudo junto então aquela aspas seria basicamente um espaço entre elas
+console.log(nomeCompleto)
