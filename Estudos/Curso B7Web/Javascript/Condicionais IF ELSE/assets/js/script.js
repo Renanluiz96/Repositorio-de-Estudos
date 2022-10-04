ea65baba209b22faeb357554850e0518c52f8e88
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-10-03 22:38:58 -0300

    Aula sobre condicionais if / else

---
+++ Estudos/Curso B7Web/Javascript/Condicionais IF ELSE/assets/js/script.js
@@ -0,0 +1,26 @@
+let idade = 17;
+
+
+//Condicional IF e Else  , Se caso o resultado for satisfeito algo vai acontecer  , se caso o resultado não for satisfeito ira acontecer outra coisa 
+if (idade > 18) { // Aqui se a variavel idade que voce declaro ali em cima for maior ( > ) que 18 ele vai mostrar uma mensagem na tela
+   console.log("Voce é maior de idade")
+}else { // Se a variavel não for maior que 18 , ou qualquer outra condição que não for satisfeita pelo if , ele vai cair no else e ira executar outra ação . Aqui no caso ira  aparecer outra mensagem
+   console.log("Voce é menor de idade")
+}
+
+//Para descobrir se alguma coisa é igual a outra voce usa o == ao inves de 1 =
+if (idade == 17 ) {
+   console.log("As idades são iguais")
+}else {
+   console.log("As idades são diferentes")
+}
+
+/*
+Sinais :
+   > Maior
+   < Menor
+   == Igual
+   >= Maior ou igual
+   <= Menor ou igual
+   != Diferente
+*/
