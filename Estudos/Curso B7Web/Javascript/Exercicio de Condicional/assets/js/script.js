6876fc308967fa39ce86ad751aad1b50c6e268c0
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-10-04 22:15:00 -0300

    Exercicio de condicional

---
+++ Estudos/Curso B7Web/Javascript/Exercicio de Condicional/assets/js/script.js
@@ -0,0 +1,19 @@
+let x = 10
+let y = 5
+console.log(x > y)
+
+let w = "10"
+let z = 10
+console.log(w === z)
+
+let a = 10
+let b = 5
+console.log(a > b)
+
+let preco = 40.3
+
+if (preco > 46) {
+   console.log(`O Preco da carne é ${preco} e esta caro`)
+}else {
+   console.log(`O preco da carne é ${preco} e esta barato`)
+}
