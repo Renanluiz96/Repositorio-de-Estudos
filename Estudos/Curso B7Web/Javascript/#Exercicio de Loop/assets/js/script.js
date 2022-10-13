ac17deb780f3fc4456c1a38bc0ec936e22cd581e
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-10-12 23:09:32 -0300

    Exercicio de Loops

---
+++ Estudos/Curso B7Web/Javascript/#Exercicio de Loop/assets/js/script.js
@@ -0,0 +1,11 @@
+let fruits = ['Maça', "Uva", 'Banana', "Laranja"]
+for(let x in fruits) {
+   //console.log(fruits[x])
+}
+
+contador = 1
+while(contador <= 100) {
+   console.log(`Contador : ${contador}`)
+   contador++
+
+}
