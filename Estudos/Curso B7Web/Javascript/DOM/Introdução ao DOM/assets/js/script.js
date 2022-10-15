670e545a2a6a21ae083ab17c0b7ab04ea6fff0a8
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-10-15 20:32:01 -0300

    Aula DOM , introdução ao DOM

---
+++ Estudos/Curso B7Web/Javascript/DOM/Introdução ao DOM/assets/js/script.js
@@ -0,0 +1,13 @@
+//DOM = (Document Object Model)
+
+// Onde voce coloca uma tag html dentro de outra , e outra tag dentro de outra fazendo assim varios caminhos , com outras tags dentro . E outra tag com outras tags dentro . Como se fosse uma arvore com varios caminhos diferentes e cada uma dentro delas contendo varios outros caminhos 
+
+//Cada tag que tem no html é basicamente um "elemento" de dom
+
+// Dom ele fica na memoria do computador é uma organização de como a pagina vai exibir , que ela fica na memoria do navegador , e que são propriedades que ficarão na memoria e serão traduzidas de forma visual para o usuario enxergar na tela. 
+//Ex: o usuario esta vendo um titulo . Para ele é somente um texto , mas para o navegador é uma tag h1 que esta dentro da tag body que esta dentro da tag html da pagina.
+//Ele guarda toda esta arvore com todas as tags e seus caminhos la na memoria do navegador e ele exibe visualmente o que esta guardado
+
+//Manipular o Dom
+// Quando falar manipular o dom , é voce vai pegar um elemento na pagina(Tag html) e vai alterar 
+//Quando eu vo la na memoria por exemplo e digo "Javascript" vai la no h1 que esta dentro do body e troca o texto dele , e quando eu faço isso automaticamente o visual vai mudar com o novo texto que voce colocou.
