42b9cca7a654bacfa169d4ac534dfd587c1566ef
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-11-04 22:52:16 -0300

    Aula de Includes e Repeat

---
+++ Estudos/Curso B7Web/Javascript/Includes e repeat/assets/js/script.js
@@ -0,0 +1,18 @@
+//Duas funções, duas novidades do ecmascript6 = Includes , Repeat
+
+//Includes = Serve para string e array , é um booleano que retorna true ou false , parecido com a função indexof , ele vai procurar um item dentro de um array, ou uma palavra dentro de uma string e ser tiver ele retorna true , se  não ele retorna false.
+//Um dos exemplos de uso é para quando voce tem um array de post, e voce quer procurar um post especifico , e voce quer saber se aquele post esta incluso na lista.
+
+// Includes no array
+//let  lista = ['ovo','cafe', 'arros', 'feijao', 'macarrao' ]
+//console.log(lista.includes('carne')) // Voce usa assim voce coloca o array um ponto a função includes e entre os parenteses voce coloca o que voce quer procurar.
+
+//Includes com strings
+//let nome  = 'Renan'
+//console.log(nome.includes('r' && 'R'))
+
+//Repeat = Função repeat ela basicamente repete o que voce quer em quantas vezes voce quer , seja uma variavel com uma string , ou uma sting em si
+
+//console.log('x'.repeat(10))
+let nome = 'renan'
+console.log(nome.repeat(3))
