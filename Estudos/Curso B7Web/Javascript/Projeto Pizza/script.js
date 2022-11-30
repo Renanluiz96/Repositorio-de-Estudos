659b5ea1d578acf750a4308ac5b131a5ef872869
Renan Luiz <93749913+Renanluiz96@users.noreply.github.com>
2022-11-30 07:05:29 -0300

    Projeto Pizza (Aula) update 3.0

--- Estudos/Curso B7Web/Javascript/Projeto Pizza/script.js
+++ Estudos/Curso B7Web/Javascript/Projeto Pizza/script.js
@@ -1,11 +1,21 @@
-//Função auxiliares para simplificar os querry selector , para não ficar fazendo o comando toda hora . é somente colocar a função e entre os parametros voce passar qual query voce quer selecionar.
+//Quantidade de pizzas do modal voce vai criar uma variavel que comeca com 1
+let modalQt = 1;
+
+let cart = [] // Variavel onde vai salvar o carrinho de compras e as informações que vão sendo adicionadas em objetos com informações de cada
+
+//Cria esta variavel para guardar as informação que voce vai pegar ao selecionar a pizza voce vai salvar ela la dentro do modal e mandar para esta variavel
+let modalKey = 0
+
+//Função auxiliares para simplificar os query selector , para não ficar fazendo o comando toda hora . é somente colocar a função e entre os parametros voce passar qual query voce quer selecionar.
 const c = (elemento) => document.querySelector(elemento) //Usa o "c" para usar o comando querySelector que retornara o item
 const cs = (elemento) => document.querySelectorAll(elemento) //Usa o "cs" para usar o querrySelectorAll que retornara um array com varios itens que voce seleciono ou varias tags ou classes
+
+//LISTAGEM DAS PIZZAS
 
 //Mapear a lista de pizza
 //Mapaear com os parametros = 1º o item que seria a pizza em si, 2º = que seria o index da pizza ou seja o numero dela .
 pizzaJson.map((item, index) => {
-    //Com o a função cloneNode voce vai clona a estrurura das pizza com as informações em gerais de cada uma, preencher todos os dados e vai mostrar na tela. Dentro do cloneNode ,voce vai usar o true para ele não pegar o proprio item e sim tudo que estiver dentro dele
+    //Com o a função cloneNode voce vai clona a estrutura das pizza com as informações em gerais de cada uma, preencher todos os dados e vai mostrar na tela. Dentro do cloneNode ,voce vai usar o true para ele não pegar o proprio item e sim tudo que estiver dentro dele
     let pizzaItem = c('.models .pizza-item').cloneNode(true); // Esta variavel pizzaItem é basicamente o clone dos items
 
     //Aqui voce vai setar um atributo data-key com o index é o numero especifico do index ou seja o id para voce reconhecer qual é cada pizza atraves deste data-key
@@ -21,14 +31,37 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('a').addEventListener('click',(e)=>{
         
         e.preventDefault();//Voce usa a função preventDefault para cancelar a ação natural de um link que é de atualizar a pagina , aqui ele vai cortar isso
+        
         let key = e.target.closest('.pizza-item').getAttribute('data-key');// aqui voce vai criar uma variavel que vai pegar a informação de qual pizza ela é através do atributo "data-key" . E a função closest é para achar o elemento mais proximo que tenha oque esta como parametro escrito ele vai procurar a partir de onde ele esteja ou dentro ou fora da div que ele tiver mais "proximo" ele vai pegar esta informação , aqui no caso eu tenho uma tag "a" e quero pegar a informação da div em que ela esta dentro . E a partir dai voce vai pegando as informações das pizzas por este atribute key e vai adicionando as informações no modal
+
+        //Sempre que abrir o modal ele vai resetar a quantidade de itens do modal
+        modalQt = 1;
+
+        //Para colocar as informações da variavel key neste outra variavel que esta fora , pois a variavel key somente esta dentro do modal , e depois que eu fecho ela , eu perco esta informação então para isto vou salvar nesta outra , para ficar salva fora ao eu selecionar a pizza ele ja selecionar e quando eu fechar eu ja possa ter esta informação guardada para tambem usar e salvar esta informação no carrinho.Ao abrir o carrinho vai pegar as informações de qual pizza é pelo key e vai me dizer qual é e salvando em modalkey
+        modalKey = key 
 
 
         //Aqui voce vai setar as informações de cada pizza antes de abrir o modal voce vai setando as informação de cada pizza especifica atraves daquela variavel que voce crio logo acima que pega o atributo data-key e mostra toda a informação especifica de cada uma e vai adicionando conforme ela pega as informação la na lista pizza.js.
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
 
+        //Para remover o item que por padrão ele ja vem selecionado que é o grande, voce vai tirar a propriedade selected dele deixar ele resetado
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+
+        //Agora para colocar o tamanho de cada uma pizza voce seleciona a div e dar um forEach(para cada um dos itens) que ele vai de um em uma div com esta class  ele vai rodar uma função
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+            //Para adicionar sempre o tamanho grande em todos os modals de pizza que voce for abrir ele vai fazer uma verificação , como o a pizza grande sempre esta em terceiro el vai fazer uma verificação e no sizeIndex 2 ele vai sempre acrescentar a classe selected , que voce reseto antes
+            if(sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+
+            //Dentro de cada item desta div vai ter um span que é onde fica o valor . Voce vai selecionar o span e acrescentar o valor nele que seria o valor que esta em sizes la no json que la vai ter o valor de cada tamanho e cada tamanho vai ter seu index ai o tamanho vai ser cada sizeIndex é um tamanho diferente e ele vai preencher os espaços
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        }) 
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         //Depois voce vai selecionar o modal que esta em pizzaWindowArea e vai setar a opacidade 0, e como ele ja esta pré configurado ja  e ta com o display none , voce só aplica um display flex para ele aparecer na tela
         c('.pizzaWindowArea').style.opacity = 0;
@@ -42,4 +75,74 @@ pizzaJson.map((item, index) => {
 
     //Agora voce pega o conteudo que voce clono e da um append la na div pizza-area que vai ser onde vai ser mostrado na tela , detealhe voce usa o append para ele ir mostrando na tela cada item um ao lado do outro , se voce usar o innerHTML ele vai ficar substituindo os itens e vai ficar com somente 1. 
     c('.pizza-area').append( pizzaItem);
+})
+
+//EVENTOS DO MODAL
+//Ações do modal voce tem que fazer fora do map pois o modal é um só  então não pode fazer dentro da listagem la do map
+
+//Função para fechar o modal
+function closeModal() {//Voce vai fazer o processo "inverso" do processo de abrir
+
+    //Voce vai setar o opacidade como 0
+    c('.pizzaWindowArea').style.opacity = 0;
+
+    //Vai dar um settimeout para ele esperar um tempo e depois colocar um display none para fechar a tela
+    setTimeout(()=> {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500)//Vai esperar meio segundo para executar esta função   
+
+
+}
+
+ //Colocando a função closeModal() em todas as divs especificas com a mesma class para que elas fechem via javascript 
+ cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{//Aqui ele vai seleciona as div com estas classes e com o forEach ele vai de item em item e vai adicionar a função de click do closemodal
+    item.addEventListener('click', closeModal)
+});
+
+//Colocando em evento de click para aumentar e diminuir a quantidade de pizza ao clicar no mais e menos
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{ //click menos
+    if(modalQt > 1) {
+        modalQt--;
+    c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+})
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{//Click mais
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+//Para alternar o botao de tamanho das pizzas para selecionar cada uma
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+    size.addEventListener('click', () =>{
+        c('.pizzaInfo--size.selected').classList.remove('selected'); // Voce vai remover a classe selected que ja continha no botão do tamanho , sendo assim a cada click um reset sempre removendo para depois ao add a cada click em lugar diferente
+        size.classList.add('selected') //e depois add a classe selected no tamanho que voce for clicar
+    })
+}) 
+
+//Addicionando as informações das pizzas ao carrinho de compras
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{//Adicionando ação ao carrinho de compras
+
+    /*Qual é a pizza? modalKey ja vai ter o id da pizza
+
+        console.log("Pizza: "+ modalKey)
+
+    //Qual é o tamanho? Voce vai saber o tamanho dela pelo data-key que voce acrescento a cada tamanho , voce vai pegar o id deste tamanho "data-key"
+    let size = c('.pizzaInfo--size.selected').getAttribute('data-key')
+    console.log('Tamanho: '+ size)
+
+    //Quantas pizzas?? Que é a informação que esta na variavel modal qt
+    console.log('Quantidade: '+ modalQt)
+    */
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    //Depois de ter pego o atributo data key para saber o tamanho voce vai acrescentando ao carrinho que é o array cart , com a função push todas as informações que serão guardada em um objeto no array, cada pizza selecionada vai ter como informação Qual pizza, Qual o tamanho e Quantidade
+    cart.push({
+        id:pizzaJson[modalKey].id,//Voce vai salvar o id da pizza pegando a informação do modalKey que esta dentro do pizzaJson voce vai acessar o id real dela com o .id
+        size, //Que seria o tamanho que voce pego com a variavel size ali em cima, voce poderia colocar ; size:size , ou somente size
+        qt:modalQt // E a quantidade da pizza que voce vai pegar na variavel modalQt
+    })
+
+    //Fechar o modal após toda ação executando a função de fechar o modal
+    closeModal()
 })
