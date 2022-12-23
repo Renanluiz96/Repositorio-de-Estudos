e7dabeb4e3dbbc5952dea5cb113bd72ece54bb6d
Renan Luiz <renanluizz1996@gmail.com>
2022-12-23 06:30:01 -0300

    Mini Projeto Urna Eletronica (AULA) update 4.0  - FIM

--- Estudos/Curso B7Web/Javascript/Projeto Urna Eletronica/script.js
+++ Estudos/Curso B7Web/Javascript/Projeto Urna Eletronica/script.js
@@ -10,7 +10,7 @@ let lateral = c('.d-1-right')
 let numeros = c('.d-1-3')
 
 
-//Variaveis de ambiente
+//VARIAVEIS DE AMBIENTE
 
 //EtapaAtual esta 0 para que ele comece pegando as informações do primeiro "objeto " do array do json e depois ele vai pulando para o proximo  objeto onde esta contendo as informações de outros candidatos
 let etapaAtual = 0;
@@ -18,12 +18,24 @@ let etapaAtual = 0;
 //Variavel para saber qual é o numero que voce esta digitando no exato momento quando voce clica ele comeca vazio. Conforme voce for preenchendo ele vai preenchendo nesta variavel aqui e vai mostrando na tela os numeros. nos quadrados 
 let numero = '' ;
 
+//Variavel para controlar o voto em branco , que comeca como um false , e depois vai fazendo a verificação para trocar ele par true mais para frente
+let votoBranco = false;
+
+//Cria um array onde voce pode salvar os votos. onde salva o titulo(vereador ou prefeito), e o numero que voce digito ou se for branco salva em branco
+let votos = []
+
+
 
 function comecarEtapa() {
     //Variavel para controlar as etapas la do json que contem as informações dos candidatos . e depois de salvar esta variavel voce vai preenchendo as informações dos candidatos atraves desta variavel que vai pegando as informações la do json
     
     //Para ir pegando as informações la do objeto json do candidato atual que começa no 0 ou seja o objeto 0 do array
     let etapa = etapas[etapaAtual];
+
+    //Toda vez que rodar a função comecar etapas para votar novamente, ou na ação de corrigir um voto , ou para votar em outro candidato , automaticamente os numeros que voce preencheu digitando vão ter que ser reiniciado e comecar de novo então no inicio desta função voce zera os numeros passando uma string vazia.
+    numero = ''
+
+    votoBranco = false ;
 
     //Aqui voce vai montar os quadrados html na tela. e depois vai incrementando mais quadrados de acordo com o for abaixo que vai incrmentar no html divs com o quadrado de acordo com o quantidade de numeros que o candidato precisa
     let numeroHtml = '';
@@ -85,7 +97,13 @@ function atualizaInterface() {
         for( let i in candidato.fotos) {
             //Faz um for para cada uma das fotos do candidato voce vai preenchendo aquela estrutura que comeco vazia , voce coloca a div html da foto ,e nela voce preenche as fotos de acordo com o propriedade do objeto
             //Cada foto que tiver voce vai preenchendo com a div que contem a foto voce vai la pega a div onde esta o candidato , e no src da imagem voce altera a foto pela foto que esta dentro do array
-            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt=""><p>${candidato.fotos[i].legenda}</p></div>`;
+
+            //Voce faz uma verificação extra para caso tiver as duas fotos do vereador e prefeito , ai a segunda imagem que seria a do vice prefeito , ela tem uma classe small na segunda div . Então se tiver as 2 div e uma delas contendo a classe small  , voce coloca a classe small nas fotos ,ficando pequena , se não mantem sem a classe small normalmente
+            if(candidato.fotos[i].url.small){
+                fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }else{
+                fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
 
 
         }
@@ -127,13 +145,64 @@ function clicou(n) {
 }
 
 function branco() {
-    alert('Clicou em Branco!')
+    numero = '' //Zera os numeros.
+    //Ao clicar no botão branco , ai a variavel que comeco como false, agora vira true
+    votoBranco = true;
+    //Depois disso voce tem que fazer igual o voto nulo , colocar apenas algumas informações e uma div onde diz Voto em branco igual o voto nulo
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '' // Tirar todo os quadrados dos numeros da tela tambem 
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    //Caso voce tenha digitado e apareceu o candidato na tela ao lado , e depois voce decidiu votar em branco , ele vai aparecer la . Voce zera o conteudo da lateral para que isto não aconteca
+    lateral.innerHTML = ''
+
+
 }
 function corrige() {
-    alert('Clicou em Corrige!')
+    //Ao clicar no botão corrige ele vai reinicia e vai somente executar  o comecaretapas() de novo.
+    comecarEtapa()
 }
 function confirma() {
-    alert('Clicou em Confirma!')
+    //Comeca com a etapa da votação como a etapa atua ou seja o 0 para poder votar nos vereadores, e depois ao final do voto ,ele vai encrementar esta variavel , e onde voce vota no prefeito.
+    let etapa = etapas[etapaAtual]; 
+
+    //Botão funcionar somente funciona em 2 ocasiões ; 1ª se o voto for branco , 2º ou se os numeros estiverem todos digitados
+    
+    //Cria uma variavel para o voto se estiver confimado, comecando ele como false. e depois que fizer a ação de verificação do voto branco ou digitado . voce transforma esta variavel para true
+    let votoConfirmado = false;
+
+    //Fazer uma verificação se o voto for branco
+    if (votoBranco === true) {
+        votoConfirmado = true; // Depois de votado em branco , então voce ja votou e pode clicar em confirma
+
+        //Quando votar em branco ele vai salvar o voto em branco la no array  mandando um objeto com o candidato e o voto em branco que voce crio onde vai ficar salvo os votos totais
+        votos.push({
+            etapa: etapas[etapaAtual].titulo, //Mandando o titulo do voto
+            voto: 'branco' //Manda a propriedade voto como branco
+        })
+    } else if(numero.length === etapa.numeros) { //ou então se a quantidade de numeros digitados , for a mesma da quantidade total de numeros que precisa votar .
+
+        votoConfirmado = true; // Depois de ter digitado todos os numeros , voce ja votou e pode clicar no botão de confirmar
+        
+        //Depois de ter digitado voce vai mandar la para o array onde contem os votos , a mesma coisa que voce mando no voto branco o titulo , e aqui vai manda o numero que voce digito do candidato
+        votos.push({
+            titulo: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    //Depois de fazer o voto voce faz uma verificação la na sua variavel votoConfirmado , se ela ja estiver como true quer dizer que ja foi votado , então voce incrementa a variavel etapa, para poder votar no proximo candidato(prefeito)
+    if(votoConfirmado) {
+        etapaAtual++;
+        //Voce faz uma verificação para ver se chego ao fim da votação . Voce faz um if para ver se a etapaAtual que voce incremento , se a etapa atual for diferente de undefined . se ela existir voce roda a a função comecarEtapa() para comecar novamente, se caso tiver undefined voce mostra dizendo que chegou ao fim
+        if(etapas[etapaAtual] !== undefined) {
+            comecarEtapa()
+        } else {
+            //Colocando aviso de fim na tela grande. 
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';;
+            console.log(votos)
+        }
+    }
 }
 
 comecarEtapa()
