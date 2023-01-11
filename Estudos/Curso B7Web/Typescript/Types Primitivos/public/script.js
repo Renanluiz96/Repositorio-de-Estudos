2e1e26be90f38396146a9b486addab5a23360830
Renan Luiz <renanluizz1996@gmail.com>
2023-01-11 07:09:07 -0300

    Create script.js

---
+++ Estudos/Curso B7Web/Typescript/Types Primitivos/public/script.js
@@ -0,0 +1,6 @@
+//Tipos primitivos , pode declarar qual vai ser o "tipo" da variavel que vai ser declarada colocando ':' depois da variavel e colocando o tipo logo depois, se voce declarar que aquela variavel vai ser de um tipo especifico, e voce colocar algum outro dado que não seja da mesma tipagem, vai acusar erro.
+var nome = 'renan'; //Vai aceitar somente string
+var idade = 90; // Somente tipo number, aceita float tambem
+var programador = true; // Tipo Boolean para true ou false
+//OBS : Na documentação do proprio typescript, se recomenda não sair colocando tipagem , em "todas" as  variaveis , pois por exemplo uma variavel nome = se supoem que vai ser colocado um nome de alguem , que vai ser do tipo string. Mesma coisa de uma variavel idade = Se propoem que vai ser colocado , um numero que seria a idade de uma pessoa.
+//Então se recomenda em voce colocar uma tipagem em alguma variavel que vai ser manipulada , ou que pode receber algum dado de input por exemplo , que ai voce vai ter que manipula-la.
