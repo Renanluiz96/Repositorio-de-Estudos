6a089e72e2fdcbec209bc769e23ea3c1e99ffd39
Renan Luiz <renanluizz1996@gmail.com>
2023-01-13 05:31:09 -0300

    Aula TypeScript [ Type de Arrays]

---
+++ Estudos/Curso B7Web/Typescript/Nova pasta/src/script.ts
@@ -0,0 +1,11 @@
+// Types em arrays = pode colocar tanto string , quanto number mais coloca um [] depois . Se fizer um array de sting ou number , e colocar outro dado que não seja respectivo dele mesmo ,ira dar erro.
+
+//Array somente de strings
+let nomes:string[] = ['renan', 'pedro', 'fulano'];
+let palavras: Array<string> = ['palavra', 'outra', 'frase'];
+
+// Array de numeros
+let idades:number[] = [90, 10, 55, 42];
+let numeros: Array<number> = [1, 2, 3, 1.44]
+
+//Alguns codigos pode ver tambem escrito diferentes formas como a segunda opção escrita com Array<Type> 
