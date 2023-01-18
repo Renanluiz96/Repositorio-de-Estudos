10a4f6cfd59e50a7f0f1e7539b1ca3b0beaa1201
Renan Luiz <renanluizz1996@gmail.com>
2023-01-18 07:14:08 -0300

    Aula de React  - Expressões em TSX

--- Estudos/Curso B7Web/React/react-vite/src/App.tsx
+++ Estudos/Curso B7Web/React/react-vite/src/App.tsx
@@ -1,9 +1,13 @@
 // Função tem que retornar alguma coisa grafica na tela. É aqui que voce cria sua aplicação inteira ,e no final voce exporta ela , para poder importar no seu arquivo principal , que la sim ira rodar o projeto em si. Aqui é só criado os componentes , e importado tudo que voce for usar , é importado para ca tambem.
 const App = () => {
+  let n1: number = 2;
+  let n2: number = 5;
+  function multiplicacao(numero1:number, numero2:number): number {
+    return numero1 * numero2
+  }
+
   return (
-    <div>
-      Ola, Mundo!
-    </div>
+    <div>Multiplicação =  {multiplicacao(5, 10)}</div>
   );
 }
 
