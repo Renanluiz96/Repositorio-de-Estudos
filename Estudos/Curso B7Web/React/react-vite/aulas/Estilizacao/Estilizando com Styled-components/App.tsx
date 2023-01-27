f232dfc62b2acb3df6d08a2e1aa84f9c4b7861b9
Renan Luiz <renanluizz1996@gmail.com>
2023-01-27 06:06:50 -0300

    Aula React - Passando props no styled- components

--- Estudos/Curso B7Web/React/react-vite/aulas/Estilizacao/Estilizando com Styled-components/App.tsx
+++ Estudos/Curso B7Web/React/react-vite/aulas/Estilizacao/Estilizando com Styled-components/App.tsx
@@ -1,10 +1,14 @@
+/*
 //Aqui estou puxando de 1 em um arquivo externo
 //import { Container } from './components/Container'
 //import { Botao } from './components/Botao'
 
 //import { Container, Botao} from './AppStyles' // Posso exportar assim do AppStyles, exporto um a um , mais se tiver muitos componentes , vai ficar muito ruim ficar exportanto assim todos , vai ficar muito grande
-
-import * as C from './AppStyles' //Usando o * estou puxando todos os componentes que estão criados aqui neste arquivo , e coloco elas como o nome C , e ao usar no componente voce coloca C. e depois do ponto o nome do componente. Recomendo usar ,pois na hora que ver um C. alguma coisa eu sei que esta falando de um "componente de estilização"
+*/
+import * as C from './AppStyles'
+/*
+//Usando o * estou puxando todos os componentes que estão criados aqui neste arquivo , e coloco elas como o nome C , e ao usar no componente voce coloca C. e depois do ponto o nome do componente. Recomendo usar ,pois na hora que ver um C. alguma coisa eu sei que esta falando de um "componente de estilização"
+*/
 
 /*
 Pode se usar o styled components de 3 maneira :
@@ -25,9 +29,12 @@ const App = () => {
     return (
         <div>
             {/*Usando os componentes , com o C. antes */}
-            <C.Container>
+            <C.Container bgcolor="orange"> 
                 Texto do componente.
-                <C.Botao>Clique aqui</C.Botao>
+
+                {/*Passando uma prop no componente, no caso da prop small , como é um boolean pode ser escrito assim . Colocando props para adaptar o componente do jeito que quisermos*/}
+                <C.Botao bg="#FF0000" >Botao grande</C.Botao>
+                <C.Botao bg="#00FF00" small>Botao pequeno</C.Botao>
             </C.Container>
         </div>
     )
@@ -35,7 +42,9 @@ const App = () => {
 
 export default App
 
+/*
 //Styled-components é a biblioteca  mais usado no mercado de trabalho atualmente. 
 //Tem que instalar ela usando o comando no terminal = npm install styled-components.
 //Como o projeto esta usando typescript, precisa tambem instalar os types dessa biblioteca = npm install -D @types/styled-components. 
 //Se usa o -D para instalaer no ambiente de desenvolvimento. Depois inicia ela normal com o npm run dev.
+*/
--- Estudos/Curso B7Web/React/react-vite/aulas/Estilizacao/Estilizando com Styled-components/AppStyles.ts
+++ Estudos/Curso B7Web/React/react-vite/aulas/Estilizacao/Estilizando com Styled-components/AppStyles.ts
@@ -1,15 +1,31 @@
-import styled from "styled-components"; //Para usar o styled-components voce importa o styled assim
+import styled from "styled-components"; 
+/*
+//Para usar o styled-components voce importa o styled assim
 
 //Aqui voce cria todos as estilização da sua pagina especifica, fica tudo guardado aqui 
 
 
 //Para criar um componente voce usa o styled. e do lado do ponto voce diz que tipo de tag html este component é aqui no caso um button . Depois do lado voce coloca a aspas para o lado `` . E dentro dela voce coloca as estilizações dos componentes. 
-export const Botao = styled.button`
-    font-size: 40px
+*/
+
+type BotaoProps = {
+    bg: string;
+    small?: boolean
+}
+/*
+Passando as props por uma tamplete string que dentro dela voce passa props como uma função anonima e ao lado usa a props. e a props que voce quer usar . passei uma prop bg que vai pega o bg la da props e vai estilizar dinamicamente com a informação de la, e uma prop small que é um boolean , passei ele como um operador ternario para simplificar o codigo, se for true ele coloca 15px , se for false ele coloca 30px. E no final criei o type para essas props acima do componente, usando o nome do componente + Props junto para facil entendimento, e usando este type voce coloca ao lado das aspas uma abre e fecha tag e entre elas coloca o nome do type <BotaoProps>
+*/
+export const Botao = styled.button<BotaoProps>`
+    font-size: ${(props) => props.small ? '15px' : '30px'};
+    background-color: ${props => props.bg};
 `;
 
-export const Container = styled.div`
-    background-color: blue;
+
+type ContainerProps = {
+    bgcolor?: string;
+}
+export const Container = styled.div<ContainerProps>`
+    background-color: ${props => props.bgcolor};
     color: white;
     padding: 20px;
 `;
