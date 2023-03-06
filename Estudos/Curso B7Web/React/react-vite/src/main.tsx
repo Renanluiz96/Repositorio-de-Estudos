afa2d2fb422c313331e4c8aa4563197439a8a9d2
Renan Luiz <renanluizz1996@gmail.com>
2023-03-06 06:35:20 -0300

    Criando primeira rota

--- Estudos/Curso B7Web/React/react-vite/src/main.tsx
+++ Estudos/Curso B7Web/React/react-vite/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom' // Importando para usar o react-router.
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter> {/* Trocando o App para o browserRouter(que vai ser a caixa para as alterações de rotas) , e dentro dela sim coloca o App */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
