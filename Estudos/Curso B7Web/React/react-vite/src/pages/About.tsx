9f40a1d03de4ce36783eb398e70b6e632633e515
Renan Luiz <renanluizz1996@gmail.com>
2023-03-06 07:16:38 -0300

    Criando links de rotas ao click para alterar na tela as configurações que ja estão carregadas no react

--- Estudos/Curso B7Web/React/react-vite/src/pages/About.tsx
+++ Estudos/Curso B7Web/React/react-vite/src/pages/About.tsx
@@ -1,9 +1,15 @@
+//Importando o link do react-router. para poder fazer um link de uma pagina pa outra sem fazer o recarregamento completo do site, ele so vai recarregar aquele componente.
+import { Link } from "react-router-dom"
+
+
 export const About = () => {
     return(
         <div>Pagina sobre
             <ul>
-                <li>Renan</li>
-                <li>Pedro</li>
+                {/*Cada li agora vai ter o componente link que vai ser visualmente igual a tag a , só que ele não vai recarregar o site completo ele somente vai alterar alterar o componente que ja esta pré carregado na memoria do react. */}
+                <li><Link to="/sobre/renan">Renan</Link></li>
+                <li><Link to="/sobre/pedro">Pedro</Link></li>
+                
             </ul>
         </div>
     )
