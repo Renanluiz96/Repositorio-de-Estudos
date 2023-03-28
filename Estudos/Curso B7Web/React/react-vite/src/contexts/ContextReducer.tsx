8d782e09789df2bf847295f6469477aa100580ca
Renan Luiz <renanluizz1996@gmail.com>
2023-03-28 06:50:55 -0300

    Criando a estrutura do Contexto com o useReducer com os types

--- Estudos/Curso B7Web/React/react-vite/src/contexts/ContextReducer.tsx
+++ Estudos/Curso B7Web/React/react-vite/src/contexts/ContextReducer.tsx
@@ -1,1 +1,44 @@
 import { createContext, useReducer } from "react";
+//Importando os dados la do arquivo reducer
+import { UserType, userInitialState, userReducer } from "../reducers/userReducer";
+import { reducerActionType } from "../types/reducerActionType";
+
+
+
+//Type que vai ter o user do reducer, que ele vai ter o mesmo type do reducer que estamos utilizando . Pode criar um objeto type que pode salvar alguns outros types ,assim ficando este type aqui como global, e dentro dele podendo ter outros types para cada reducer por exemplo
+type initialStateType = {
+    //Criar um type que vai ser usado neste reducer, que poderia ter mais de 1 reducer , e este type vai ser o type exportao junto com a constante que ja tem salvo os dados.
+    user: UserType
+}
+
+// Criando um type para o contexto que vai tertypes mais genericos.
+type ContextType = {
+    state: initialStateType;
+    dispatch: React.Dispatch<any>;
+}
+
+
+//Criar uma constante que vai ter os dados iniciais do state.
+const initialState = {
+    //Como pode ter mais de um reducer eu coloco um user com uma tipagem do reducer que quero usar, se tiver outro reducer pode colocar um outro dado com a tipagem para outro reducer
+
+    user: userInitialState //Este user vai ser usado em um reducer, por isso que coloquei esta tipagem , pois la ja vai ter acesso ao name e age que são os dados que quero usar.
+}
+
+
+//Criando um contexto utilizando reducers. Mandando dentro do createContext dois parametros o primeiro que seria o dado da state, e o segundo seria o dispatch(que é a função do reducer, para alteração do reducer por ações.)
+
+export const ContextReducer = createContext<ContextType>({
+    //State com o type que vai ter aquela state "global" que dentro desta state ja tem a deste reducer, podendo ter até mais.
+    state: initialState,
+    
+    //Tem um dispatch que vai ter uma função que pode retornar null
+    dispatch: () => null 
+});
+
+//Criando um reducer que engloba todos os outros reducers, pois somente pode usar 1 reducer no fim das contas.
+//Reducer global - que vai receber por parametros a state com o type global de state. e o actio que vai ter um type para action externo
+const mainReducer = (state:initialStateType, action: reducerActionType) => ({
+    //Aqui vai ter este reducer que vai ter uma função do reducer criada externamente, utilizando  o dado do proprio usuario que foi pego tambem em um arquivo externo e tipado para este reducer.
+    user: userReducer(state.user, action)
+});
--- Estudos/Curso B7Web/React/react-vite/src/reducers/userReducer.ts
+++ Estudos/Curso B7Web/React/react-vite/src/reducers/userReducer.ts
@@ -19,9 +19,11 @@ export const userInitialState = {
 export const userReducer = (state: UserType, action: reducerActionType) => {
     //Dentro do reducer cria um switch que é padrão do reducer, para cada type fazer uma action diferente.
     switch(action.type) {
+        //Vai ter um type chamado CHANGE_NAME. que ao ser acionado ele vai retornar um objeto, que dentro dele vai ter uma copia do state(por causa da imutabilidade , tem que fazer uma copia do state e alterar nela.) e nessa copia voce altera o valor de name, para o valor do payload.name ou seja o que voce quiser mandar ou o que digito agora vai ser alterado
         case 'CHANGE_NAME' :
             return {...state, name: action.payload.name}
         break
+        //Mescma coisa com o age , vai ser retornado uma copia da state , e alternado o valor da copia para o que voce mandar no payload.
         case 'CHANGE_AGE' :
             return {...state, age: action.payload.age}
         break
