5544d2bb9163d46a1d9c404a0d1c73f41742e072
Renan Luiz <renanluizz1996@gmail.com>
2023-03-31 06:20:58 -0300

    Pegando as informações do reducer no Redux, e Mudando as informações no reducer

--- Estudos/Curso B7Web/React/react-vite/src/App.tsx
+++ Estudos/Curso B7Web/React/react-vite/src/App.tsx
@@ -1,5 +1,3 @@
-
-
 /* Redux - É um Gerenciador de estados igual ao ContextApi , só que o Redux é mais robusto , tem mais funcionalidades.
 
 Normalmente usado em projetos um pouco mais antigos de 2019 para tras usavam muito o redux para gerenciar os dados de  componentes e salvar em um lugar só chamado "storage" e esses dados salvos seriam salvos em reducers que ficariam armazenados para cada situação , seja ela os dados do usuario logado após o preenchimento de algum formulario ou login, seja ele um darkmode , com informações caracteristicas do site em um estado e ao click alterava toda a estetica do site com as propriedades guardadas em um reducers que estaria salvo dento do store do redux.
@@ -36,16 +34,43 @@ Normalmente se cria uma pasta chamada redux , e dentro desta pasta se coloca tod
 Sempre no redux comeca criando os reducers primeiro ou seja a pasta reducers e cada reducer como arquio .ts do reducer em especifico, ou seja quer criar um reducer de usuario cria userReducer.ts .
 Depois de criado os reducers, voce precisa cria e configurar o store. Cria um arquivo dentro da pasta do redux mesmo , chamado store.ts .
 
+Para usar o reducer na sua aplicação voce usa o hook useSelector, ou pode criar uma pasta chamada hooks para criar um hook geral , e la voce configura o hook e coloca um type geral , para que no app principal voce apenas import o hook e usa ele na aplicação , ao inves de fazer muitos imports em cada tela que voce quer, isso de criar uma pasta para concentrar toda a informação em um arquivo só é bem usado quando se tem muito  reducers diferente, e voce quer usa-los em uma mesma pagina, isso iria gerar muitos imports. Criando um arquivo voce faz tudo la e exporta apenas o hook ja configurado e tipado para onde voce quer usar.
+
 */
 
+//Hook useSelector tipado com type geral.
+import { useAppSelector } from "./hooks/useAppSelector"
+//Hook para o uso do dispatch para ele executar as actions do reducers.
+import { useDispatch } from "react-redux"
+import { setName, setAge } from "./redux/reducers/userReducer";
+
 const App = () => {
+  //Usando o hook do reducer pegando apenas a informação do reducer que quero usar apena retornando o state.user ou outro reducer que eu tiver eu uso o state.outroreducer. Que eu ja vou ter acesso a todas informações do reducer.
+  const user = useAppSelector(state => state.user)
+
+  //Salvei o hook useDispatch em uma função dispatch, passando dentro dela na hora que for usar somente o nome ação e o valor novo que quer adicionar.
+  const dispatch = useDispatch();
+
+  //Criando função auxiliar para simplificar o codigo do dispatch . Para não precisar ficar toda hora que for usar usa o dispatch dentro dela a ação  e depois o valor novo. 
+  const changeName = (newName: string) => dispatch(setName(newName))
+  const changeAge = (newAge: number) => dispatch(setAge(newAge))
+
+  //Função para setar os valores do input , para alterar o valor dos reducers de acordo com o que for digirando pegando o targer.value do que foi digitado. E usando a função auxiliar criada para simplificar o codigo.
+  const handleNameInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+    changeName(e.target.value)
+  }
+  const handleAgeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+    changeAge(parseInt(e.target.value))
+  }
+
   return (
     <div>
-      Meu nome é : ... e tenho ... anos <br />
+      Meu nome é : {user.name} e tenho {user.age} anos <br />
       Tema: ...
 
       <hr />
-      <input type="text" value={',,,'}/>
+      <input type="text" value={user.name} onChange={handleNameInput}/>
+      <input type="text" value={user.age} onChange={handleAgeInput}/>
 
       <hr />
       <button>Switch Theme</button>
