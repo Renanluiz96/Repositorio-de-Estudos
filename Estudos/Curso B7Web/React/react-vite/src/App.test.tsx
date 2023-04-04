8e476914dc1287d4b6c4411e60dae2a8f64639e7
Renan Luiz <renanluizz1996@gmail.com>
2023-04-04 06:18:43 -0300

    Aula de Testes - Configurando o vite com vitest, e comecando a aula com alguns testes unitarios

---
+++ Estudos/Curso B7Web/React/react-vite/src/App.test.tsx
@@ -0,0 +1,8 @@
+it('should add 1 to 1 and return 2', () => {
+    let result = 1 + 2 ;
+    expect(result).toBe(3)
+})
+it('should multiply 5 by 3 and return 15', () => {
+    let result = 5 * 3;
+    expect(result).toBe(15)
+})
--- Estudos/Curso B7Web/React/react-vite/vite.config.ts
+++ Estudos/Curso B7Web/React/react-vite/vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    },
 })
