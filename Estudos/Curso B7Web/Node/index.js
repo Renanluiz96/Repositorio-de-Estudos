dc931ece9b0484aa9fc5c33173fdfbc5b5707188
Renan Luiz <renanluizz1996@gmail.com>
2023-04-18 06:35:22 -0300

    Instalando e configurando  o typescript no node

--- Estudos/Curso B7Web/Node/index.js
+++
@@ -1,7 +0,0 @@
-let nome = "Renan"
-let idade = 26
-let aula = "Node"
-console.log(`Meu nome é ${nome} tenho ${idade} e estou iniciando meu estudo em $
-${aula} este é o meu primeiro  projeto.`)
-
-//Para rodar o arquivo no terminal usa o comando + nome do arquivo a ser executado .  Ex : node index.js 
---
+++ Estudos/Curso B7Web/Node/src/index.ts
@@ -0,0 +1,11 @@
+// Para rodar o arquivo no terminal usa o comando + nome do arquivo a ser executado .  Ex : node index.js 
+// Tem que instalar uma dependencia de types para node para usar o node com o typescript - npm install --save-dev @types/node. no terminal.
+// Para rodar o typescript para ficar monitorando voce tem que deixar um terminal somente para ele ficar observandos as alterações e a cada configuração alterada, ele converter no arquivo js que seria o arquivo final para isso tem que fazer o comando no terminal  = tsc -w 
+//Para fazer scripts no  package.json para simplificar o codigo no terminal ao digitar comandos. cria um script "start": node/dist.js . e um script watch-ts: tsc -w. Para usar usa junto com o "run" , Ex = npm run start , npm run watch-ts.
+
+let nome: string = "Renan"
+let idade: Number = 26
+let aula: string = "Node"
+
+console.log(`Meu nome é ${nome} tenho ${idade} e estou iniciando meu estudo em 
+${aula} este é o meu primeiro  projeto.`)
