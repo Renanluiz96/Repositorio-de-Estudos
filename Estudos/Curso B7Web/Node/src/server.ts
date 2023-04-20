5ac764a92ae79d20ad439aadc7b2dcef102f296a
Renan Luiz <renanluizz1996@gmail.com>
2023-04-20 06:44:01 -0300

    Aula Node - Criando pastas e arquivos estaticos.

--- Estudos/Curso B7Web/Node/src/server.ts
+++ Estudos/Curso B7Web/Node/src/server.ts
@@ -1,12 +1,21 @@
 //Servidor - vai usar o arquivo de rotas que foi criado separadamente, e este arquivo "é apenas para o uso das rotas que foram criados externamente."
 import express, { Response, Request } from 'express';
+//Importando uma lib path para usar para que quando crie pastas e arquivos estaticos ele ja salva o caminho da pasta 
+import path from 'path';
+
 //Importando o arquivo de rota
 import mainRoutes from './routes/index'
 import painelRoutes from './routes/painel'
 
 const server = express();
 
-//Para usar o arquivo do servidor aqui voce usa a função use() que ja tem no express para uso de arquivos de rotas externos. Usando assim ,voce ja vai ter acesso a todas as rotas configuradas naquele arquivo. A partir do / vai ser usado as rotas que estão neste arquivo.
+//Criando pasta e arquivos estaticos
+// atraves da pasta public , para o usuario ter acesso atraves do navegador. Bem usado quando quer pegar algum arquivo que eu tenho salvo atraves da url. Vai usar o .use , e dentro dele vai usar uma função do express chamada static e dentro dela voce passa a rota do pasta public, e usa a lib path, para salvar onde ficara a pasta public .
+server.use(express.static(path.join(__dirname, '../public')));
+
+
+//Para usar o arquivo do servidor aqui voce usa a função use()
+// que ja tem no express para uso de arquivos de rotas externos. Usando assim ,voce ja vai ter acesso a todas as rotas configuradas naquele arquivo. A partir do / vai ser usado as rotas que estão neste arquivo.
 server.use('/', mainRoutes);
 
 //Para usar rotas especificas de um lugar só exemplo do painel . voce usa a rotas e no primeiro parametro , voce passa da onde quer que ele comeca a partir ou seja a partir do /painel que ai ele comeca a usar as rotas que foi criada deste arquivo especifico.
