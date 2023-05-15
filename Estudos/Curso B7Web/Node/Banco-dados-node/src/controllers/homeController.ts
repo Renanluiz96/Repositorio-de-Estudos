8069e778b7b63c65f3e28aa4e3ed4b83f09230ca
Renan Luiz <renanluizz1996@gmail.com>
2023-05-15 06:26:04 -0300

    Inserindo a estrutura para aula de banco de dados em node

---
+++ Estudos/Curso B7Web/Node/Banco-dados-node/src/controllers/homeController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+import { Product } from '../models/Product';
+
+export const home = (req: Request, res: Response)=>{
+    let age: number = 90;
+    let showOld: boolean = false;
+
+    if(age > 50) {
+        showOld = true;
+    }
+
+    let list = Product.getAll();
+    let expensiveList = Product.getFromPriceAfter(12);
+
+    res.render('pages/home', {
+        name: 'Bonieky',
+        lastName: 'Lacerda',
+        showOld,
+        products: list,
+        expensives: expensiveList,
+        frasesDoDia: []
+    });
+};
