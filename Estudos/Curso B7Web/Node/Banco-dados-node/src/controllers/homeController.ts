fa1c6ca2dea53a332b865bbfd1fe59ec774bf818
Renan Luiz <renanluizz1996@gmail.com>
2023-05-16 06:22:54 -0300

    Fazendo a conexao com o MySql

--- Estudos/Curso B7Web/Node/Banco-dados-node/src/controllers/homeController.ts
+++ Estudos/Curso B7Web/Node/Banco-dados-node/src/controllers/homeController.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from 'express';
 
+// Importando a configuração da conexão do banco de dados que foi feita la no arquivo mysql. para uso aqui no controller.
+import { sequelize } from '../instances/mysql';
+
 import { Product } from '../models/Product';
 
-export const home = (req: Request, res: Response)=>{
+export const home = async (req: Request, res: Response)=>{
+    // Fazendo um try catch para testar se a conexão com o banco de dados deu certa. Com a função authenticate que ja é do proprio sequelize para teste mesmo.
+    try {
+        await sequelize.authenticate()
+        console.log('Conexão estabelecida com sucesso!')
+    } catch (error) {
+        console.log('Deu problema: ', error)
+    }
+
+
     let age: number = 90;
     let showOld: boolean = false;
 
