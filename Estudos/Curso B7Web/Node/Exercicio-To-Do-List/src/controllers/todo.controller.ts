6a3fec6aa60de57a60bd334bed4dc7894b37cec6
Renan <renanluizz1996@gmail.com>
2023-08-23 06:49:56 -0300

    Aula Node - Exercicio To-do-List = Listando as tarefas

--- Estudos/Curso B7Web/Node/Exercicio-To-Do-List/src/controllers/todo.controller.ts
+++ Estudos/Curso B7Web/Node/Exercicio-To-Do-List/src/controllers/todo.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { Sequelize } from "sequelize";
 import { Todo } from "../models/Todo";
 
-export const all =async () => {
-    
+export const all = async (req: Request, res: Response) => {
+    const list  = await Todo.findAll();
+    res.json({ list })
 }
 
 export const add =async () => {
