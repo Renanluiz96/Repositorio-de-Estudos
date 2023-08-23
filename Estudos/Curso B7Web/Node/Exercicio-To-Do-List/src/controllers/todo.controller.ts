21b71140d2f403f8373631cef0d6228395774b1c
Renan <renanluizz1996@gmail.com>
2023-08-23 06:30:06 -0300

    Aula Node - Exercicio To-do-List = Criando as Rotas.

---
+++ Estudos/Curso B7Web/Node/Exercicio-To-Do-List/src/controllers/todo.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { Sequelize } from "sequelize";
+import { Todo } from "../models/Todo";
+
+export const all =async () => {
+    
+}
+
+export const add =async () => {
+    
+}
+
+export const update =async () => {
+    
+}
+
+export const remove =async () => {
+    
+}
