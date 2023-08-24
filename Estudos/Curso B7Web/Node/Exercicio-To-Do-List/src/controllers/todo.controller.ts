c3c366f07565fd0510605895194f449025bbac81
Renan <renanluizz1996@gmail.com>
2023-08-24 06:03:29 -0300

    Aula Node - Exercicio To-do Simples  = Atualizando a tabela independente , se for um dado ou varios dados , vai ser feito uma verificação de quais dados foram alterados, e alterar somente o dado alterado pelo body da requisição

--- Estudos/Curso B7Web/Node/Exercicio-To-Do-List/src/controllers/todo.controller.ts
+++ Estudos/Curso B7Web/Node/Exercicio-To-Do-List/src/controllers/todo.controller.ts
@@ -25,10 +25,37 @@ export const add = async (req: Request, res: Response) => {
     }
 }
 
-export const update =async () => {
-    
+export const update =async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+
+    let todo = await Todo.findByPk(id);
+    if (todo) {
+        
+        if (req.body.title) {
+            todo.title  = req.body.title;
+        }
+
+        if (req.body.done) {
+            switch (req.body.done.toLowerCase()) {
+                case 'true':
+                case '1':
+                    todo.done = true;   
+                    break;
+                case 'false':
+                case '0':
+                    todo.done = false;
+                    break;
+            
+            }
+        }
+
+        await todo.save();
+        res.json({ item: todo})
+    } else{
+        res.json({ error: 'Item não encontrado' })
+    }
 }
 
-export const remove =async () => {
+export const remove =async (req: Request, res: Response) => {
     
 }
