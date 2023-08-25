bc130a1c5714ff59a513f8d0c2ffb712efa6a1f8
Renan <renanluizz1996@gmail.com>
2023-08-25 07:07:43 -0300

    Aula Node - Upload Arquivos e imagens = Intalando a lib multer. e Configurando o upload , e acrescentando alguns comentarios com algumas explicações.

--- Estudos/Curso B7Web/Node/Upload-arquivos-imagens/src/routes/api.ts
+++ Estudos/Curso B7Web/Node/Upload-arquivos-imagens/src/routes/api.ts
@@ -1,27 +1,32 @@
 import { Router } from "express";
+// Importando a lib externa para manipular imagens aqui usamos a multer - npm install --save multer. npm instal @types/multer.
+import multer from "multer";
 
 import * as ApiController from '../controllers/apiController'
 
+// Configurar o multer ,salvando a função em uma variavel para uso. Dentro dela tem alguns parametros para usar , colocando dentro de um objeto.
+const upload  = multer({
+    // dest é pra onde ele vai enviar o arquivo , ele vai criar uma pasta se caso não tiver, ele ira criar uma pasta na raiz do servidor onde ta o server.ts , não onde e a pasta de routes. usando o ./ . Voce pode mandar para esta pasta temporaria digamos assim para manipular o arquivo. Ou envia para a pasta public , ja para disponibilizar para o publico depois de processado na pasta temporaria, voce envia e deixa publico bonitinha para o publico.
+    // Para enviar o arquivo tem que criar uma rota via post . Voce cria la em baixo . Que ao enviar no body , ele enviara para a pasta que voce coloco aqui no dest , seja ela temporaria ou não. Voce tem que criar uma função para ser enviada na requisição la no controller. Uma função que retorna um res.json . 
+    dest: './temporario' 
+})
+
 const router = Router()
 
-// Criando as rotas da api 
 
-// Criando um endpoint ping-pong, onde ao acessar a rota ele vai fazer um teste para ver se a rota esta funcionando.
 router.get('/ping', ApiController.ping);
-
-// Endpoint onde ira sortear um numero e me retornara este numero na tela
 router.get('/random' , ApiController.random);
-
-// Endpoint onde me retornara o algum nome que eu passar na url usando o params com o : na url para mostrar nomes dinamicos via url na tela
 router.get('/nome/:nome', ApiController.nome);
 
-
-// Rotas fixas são colocadas primeiras, e logo apos coloca a rota dinamica. Para uma rota não atrapalhar a outra . Se coloca a rota fixa depois da dinamica , pode não achar, pois pode cair na rota dinamica acidentalmente.
 router.post('/frases', ApiController.createPhrase)
 router.get('/frases', ApiController.listPhrases)
 router.get('/frase/aleatoria', ApiController.randomPhrase)
 router.get('/frase/:id' ,ApiController.getPhrase)
 router.put('/frase/:id', ApiController.updatePhrase)
 router.delete('/frase/:id', ApiController.deletePhrase)
+
+// Rota para enviar o arquivo. Cria uma função no controller que retorna um res.json.
+// E nessa rota voce coloca um 3° parametro que seria aquela função do multer que voce configuro la em cima.  1° parametro a rota, 2° parametro o callback de upload , um lidador(manda o tipo de callback que quer enviar , single e uma delas e dentro dele voce coloca uma string , usando o nome que voce espera mandar la ele envia 1 arquivo , la no postman voce envia um arquivo com a key que voce mandar aqui. pode ser um avatar ai voce coloca o nome avatar, e manda o arquivo ao lado no campo new file. Mais tem que ser o mesmo nome passado aqui na string e la na key o mesmo nome .), 3° função que executa ao acionar a rota. O callback ja acontece antes de chegar na rota , o arquivo ja vai estar la na pasta antes de chegar no arquivo.
+router.post('/upload', upload.single('teste'), ApiController.uploadFile)
 
 export default router;
