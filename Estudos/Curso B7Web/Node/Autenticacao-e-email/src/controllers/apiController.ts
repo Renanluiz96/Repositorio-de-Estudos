73f345f944e9612be600ce8244c5a9f0c4649e34
Renan <renanluizz1996@gmail.com>
2023-09-20 06:30:12 -0300

    Aula Node - Autenticação e email = Gerando o token JWT com a lib jsonwebtoken , e emplementando no login do controller .

--- Estudos/Curso B7Web/Node/Autenticacao-e-email/src/controllers/apiController.ts
+++ Estudos/Curso B7Web/Node/Autenticacao-e-email/src/controllers/apiController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+import JWT from 'jsonwebtoken'; // Biblioteca para utilizar autenticação JWT , npm install jsonwebtoken. E tambem tem os types
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
@@ -35,7 +41,20 @@ export const login = async (req: Request, res: Response) => {
         });
 
         if(user) {
-            res.json({ status: true });
+            // Criando um token JWT ao momento que voce logou na sua conta ,ele criara um token contendo as informações da sua conta em uma chave privada que fica no seu sistema.
+            const token = JWT.sign( // A função sign tem 3 parametros  =
+            // 1° um objeto onde vai conter as informações do usuario , ou algo a mais que queira acrescentar. 
+            // 2° Chaver privada (obrigatoria) = que fica salva la nas variaveis de ambiente, para usar tem que exporta o .env aqui, voce usa essa chave( que seria uma string onde vai conter um hash ou algum conjunto de caracteres que voce queira) para cripografar e descriptografar o objeto que vai conter o token, essa chave privada fica salva la nas variaveis de ambiente.
+            // 3° Um objeto contendo a data de expiração que este token pode expirar.
+
+                { id: user.id, email: user.email}, // Salvando o id e o email do usuarios apenas em um objeto
+                process.env.JWT_SECRET_KEY as string, // Chave privada que esta salva la na variavel de ambiente , tem que colocar as string para dizer que é uma string se não da erro no typescript.
+                { expiresIn: '2h' }
+
+            )
+
+            // E quando tiver usando o JWT , no final tem que retornar tambem o token.
+            res.json({ status: true, token });
             return;
         }
     }
