06908e5334b77e97d9134950f14197be191fda85
Renan <renanluizz1996@gmail.com>
2023-11-23 08:01:39 -0300

    Aula Node - Testes  = Implementando o service no controller.

--- Estudos/Curso B7Web/Node/Testes-node/src/controllers/apiController.ts
+++ Estudos/Curso B7Web/Node/Testes-node/src/controllers/apiController.ts
@@ -1,26 +1,30 @@
 import { Request, Response } from 'express';
-import { User } from '../models/User';
+import * as UserService from '../services/UserService' // Não importa mais o model aqui , importa o service para uso das funções respectivas do usuario aqui . la voce cria todas as funções aqui voce somente importa as funções para o "USO".
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
 }
 
+// Fazendo isso substituindo toda a verificação e criação do usuario que antes era feita aqui por apenas usar um servico la do service  , e apenas utilizando aqui  , pois aqui é somente o uso , em um projeto real não é feito esta parte no controller e sim no service.
 export const register = async (req: Request, res: Response) => {
-    if(req.body.email && req.body.password) {
-        let { email, password } = req.body;
+    if(req.body.email && req.body.password) { // Se mando no body da requisição o email e o password
+        let { email, password } = req.body; // Pegando o email e o password do body e salvando em variaveis.
 
-        let hasUser = await User.findOne({where: { email }});
-        if(!hasUser) {
-            let newUser = await User.create({ email, password });
 
+        const newUser = await UserService.createUser(email, password); // Usando a função do service aqui para criar um novo usuario com os dados enviador do body que foram salvos em variaveis . Ai la no service que vai ser feito todo procedimento , se tiver algum usuario com os dados ele retorna um erro , caso não tiver ele ira criar um usuario .
+
+        //Como ao criar um usuario ele ira retornar ou o usuari ou um erro . Voce verifica se ele retorno um erro , voce envia o proprio erro no json , caso o contrario que deu certo voce envia o status 201 e o id do usuario.
+        if(newUser instanceof Error) { 
+            res.json({ error: newUser.message })
+        }else {
             res.status(201);
-            res.json({ id: newUser.id });
-        } else {
-            res.json({ error: 'E-mail já existe.' });
+            res.json({ id: newUser.id })
         }
+
+    } else{
+        res.json({ error: 'E-mail e/ou senha não enviados.' });
     }
 
-    res.json({ error: 'E-mail e/ou senha não enviados.' });
 }
 
 export const login = async (req: Request, res: Response) => {
@@ -28,11 +32,10 @@ export const login = async (req: Request, res: Response) => {
         let email: string = req.body.email;
         let password: string = req.body.password;
 
-        let user = await User.findOne({ 
-            where: { email, password }
-        });
+        const user = await UserService.findByEmail(email); // Procura o email passado la no banco de dados e retorna o email
 
-        if(user) {
+        //Depois verifica se o email que ele pego la no banco de dados , e o password digitado pelo usuario no login , for o mesmo password que esta encriptografado , ele ira retornar true e liberara o acesso.
+        if(user && await UserService.matchPassword(password, user.password)) { // Conferindo se a senha do usuario digitada e a senha incriptografada são as mesmas.
             res.json({ status: true });
             return;
         }
@@ -42,7 +45,7 @@ export const login = async (req: Request, res: Response) => {
 }
 
 export const list = async (req: Request, res: Response) => {
-    let users = await User.findAll();
+    let users = await UserService.all(); //Pegando os usuarios usando a função all que ira pegar todos os usuarios listados no banco
     let list: string[] = [];
 
     for(let i in users) {
